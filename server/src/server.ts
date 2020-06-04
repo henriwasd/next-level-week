0474a220d39b7e588fed0974fa4da25facd807fe
Henrique Silva <henriwasd@outlook.com>
2020-06-04 15:13:16 -0300

    Continuando a semana dev

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,15 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('Listagem');
+
+  response.json([
+    'Diego',
+    'cleiton',
+    'henrique'
+  ]);
+});
+
+app.listen(3333);
